4e4d7e81bd3ab92cba0e3d605fd8a1f05c9f88d5
jay prakash <iamjayprakashdewangan2004@gmail.com>
2024-12-14 23:47:58 +0530

    worker profile creation done

--- constants/utils.js
+++ constants/utils.js
@@ -5,15 +5,17 @@ const BASE_URL = "https://weconnect-s060q7i6.b4a.run/"
 
 
 const getTokens = async () => {
+  let token = ""
   try {
-    const token = await AsyncStorage.getItem(BASE_URL)
-    return await AsyncStorage.getItem('accessToken')
+     token =  await AsyncStorage.getItem('accessToken')
+    console.log(token)
+    return token
 
 
   } catch (error) {
     console.log(error)
   }
-  return null
+  return token
 }
 const getUserDetails = async (accessToken = getTokens()) => {
   // https://weconnect-s060q7i6.b4a.run/api/v1/user/get_user_details/
@@ -41,6 +43,7 @@ const getUserDetails = async (accessToken = getTokens()) => {
 }
 
 const getWorkerDetails = async (accessToken = getTokens()) => {
+
   try {
     const res = await axios.get(BASE_URL + "/api/v1/user/get_worker_details/", {
       headers: {
@@ -84,6 +87,37 @@ const getWorkerDetails = async (accessToken = getTokens()) => {
   return null;
 };
 
+const setWorkerDetails = async (name, profileImg, lat, long, city, pin_code, state, bio,experience, accessToken = getTokens())=>{
+  console.log(name, accessToken);
 
+  const formData = new FormData();
 
-export { BASE_URL, getTokens, getUserDetails,getWorkerDetails }
+  formData.append("name", name); // Key: "name", Value: name
+  formData.append("profileImg", {
+    uri: profileImg, // The local file path
+    name: "profile.jpg", // Desired filename
+    type: "image/jpeg", // MIME type
+  }); // Key: "profileImg", Value: { uri, name, type }
+  formData.append("lat", lat); // Key: "lat", Value: lat
+  formData.append("long", long); // Key: "long", Value: long
+  formData.append("city", city); // Key: "city", Value: city
+  formData.append("state", state); // Key: "state", Value: state
+  formData.append("pin_code", pin_code); // Key: "pin_code", Value: pin_code
+  formData.append("bio", bio); // Key: "bio", Value: bio
+  formData.append("experience", experience); // Key: "experience", Value: experience
+
+      try {
+      const res = await axios.patch(BASE_URL + "/api/v1/user/create_worker/", formData, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+  
+      console.log("Update Response:", res.data);
+      return res.data
+    } catch (error) {
+      console.error("Error updating user details:", error.response || error);
+    }
+  };
+export { BASE_URL, getTokens, getUserDetails,getWorkerDetails,setWorkerDetails }
